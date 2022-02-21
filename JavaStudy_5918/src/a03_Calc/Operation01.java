da57ff19e18f6bdc0951a62af0eb113255b14144
ITPS <ITPS@gmail.com>
2022-02-21 12:48:31 +0900

    연산자1

---
+++ JavaStudy_5918/src/a03_Calc/Operation01.java
@@ -0,0 +1,14 @@
+package a03_Calc;
+
+public class Operation01 {
+
+	public static void main(String[] args) {
+		int score = 10;
+		System.out.println(score);
+		System.out.println(score);
+		System.out.println(--score);
+		System.out.println(score++);
+		System.out.println(score);
+	}
+
+}
